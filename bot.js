d29ae7c5ed6cd9dd8d37578cb68570eed0348981
dhtdht020 <dhtdht020@gmail.com>
2017-12-31 17:18:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,12 +6,12 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === '!ping') {
     	message.reply('pong');
   	}
 });
 client.on('message', message => {
-    if (message.content === 'dyno') {
+    if (message.content === '!dyno') {
     	message.reply('is a stegosaurous');
   	}
 });
